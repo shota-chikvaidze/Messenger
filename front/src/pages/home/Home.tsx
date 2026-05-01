3278b8937039b40bad18ff77b4bf42bcedb51290
shota chikvaidze <shchikvaidze1@gmail.com>
2026-05-01 18:53:12 +0400

    image improvements

--- front/src/pages/home/Home.tsx
+++ front/src/pages/home/Home.tsx
@@ -17,14 +17,14 @@ export const Home = () => {
         <div className="w-full relative rounded-2xl overflow-hidden max-h-[80vh] ">
 
           <div className="absolute inset-0 bg-black/35 md:bg-black/15 rounded-3xl " />
-          <img src={textImg} alt='Girl texting on phone' className='h-[440px] w-full object-cover sm:h-[480px] md:h-auto' />
+          <img src={textImg} loading='eager' alt='Girl texting on phone' className='h-[440px] w-full object-cover sm:h-[480px] md:h-auto' />
 
           <div className='absolute left-4 top-1/2 max-w-[240px] -translate-y-1/2 space-y-4 xs:left-18 xs:max-w-[350px] md:left-36'>
             <h1 className='text-4xl text-white xs:text-5xl md:text-7xl'> Message <br /> privatly </h1>
 
             <p className=' text-white leading-5 md:text-md '> Simple, reliable, private messaging and calling for free*, available all over the world. </p>
 
-            {!isInitialized ? null : !user && (
+            {isInitialized && !user && (
               <Link to={'/login'}>
                 <button className="cursor-pointer mt-8 transition-all bg-blue-500 text-white px-6 py-2 rounded-lg border-blue-600 border-b-[4px] hover:brightness-110 active:border-b-[2px] active:brightness-90 active:translate-y-[2px]">
                   Log in
@@ -47,7 +47,7 @@ export const Home = () => {
 
           <div className='w-full max-w-[370px] md:w-auto'>
 
-            <img src={messagesImage} alt='Group messages' className='h-auto w-full rounded-xl object-cover md:w-[370px]' />
+            <img src={messagesImage} loading='lazy' alt='Group messages' className='h-auto w-full rounded-xl object-cover md:w-[370px]' />
 
           </div>
 
@@ -61,6 +61,7 @@ export const Home = () => {
             <img
               src={conversationImage}
               className='w-full rounded-2xl object-cover md:w-auto'
+              loading='lazy'
               alt='Messages image'
             />
           </div>
